fbb3e94a26a242ff11e4ad15c5dd74879d0c3be1
Otavio Cesar <o.caesar@hotmail.com>
2022-04-04 17:20:28 -0300

    adjusting environment variables

--- src/infra/environments/constants.example.ts
+++ src/infra/environments/constants.example.ts
@@ -1,5 +1,8 @@
 export const constants = {
+  node_env: '',
   secret_jwt: '',
-  mongo_password: ''
+  mongo_user: '',
+  mongo_password: '',
+  mongo_host: '',
+  mongo_database: '',
 };
-
--- src/infra/environments/index.ts
+++ src/infra/environments/index.ts
@@ -2,17 +2,16 @@ import * as dotenv from 'dotenv';
 import { constants } from './constants';
 
 dotenv.config();
-const NODE_ENV: string = process.env.NODE_ENV || 'development';
+const NODE_ENV: string = process.env.NODE_ENV || constants.node_env;
 
 const PORT: number = +process.env.PORT || 3000;
 
 const SECRET_JWT = process.env.SECRET_JWT || constants.secret_jwt;
 
-const MONGO_USER = process.env.MONGO_USER || 'db_mongo';
+const MONGO_USER = process.env.MONGO_USER || constants.mongo_user;
 const MONGO_PASS = process.env.MONGO_PASS || constants.mongo_password;
-const MONGO_HOST = process.env.MONGO_HOST || 'cluster0.bs8ci.mongodb.net';
-const MONGO_DATABASE =
-  process.env.MONGO_DATABASE || 'myFirstDatabase?retryWrites=true&w=majority';
+const MONGO_HOST = process.env.MONGO_HOST || constants.mongo_host;
+const MONGO_DATABASE = process.env.MONGO_DATABASE || constants.mongo_database;
 const MONGO_URL =
   process.env.MONGO_URL ||
   `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DATABASE}`;
