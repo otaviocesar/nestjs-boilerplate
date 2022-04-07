4e56cf7c218ae76c962b0fb89b55f2cf1a30612f
Otavio Cesar <o.caesar@hotmail.com>
2022-04-07 14:45:46 -0300

    Ajustando docker compose e adicionando .env

--- src/infra/environments/constants.example.ts
+++
@@ -1,8 +0,0 @@
-export const constants = {
-  node_env: '',
-  secret_jwt: '',
-  mongo_user: '',
-  mongo_password: '',
-  mongo_host: '',
-  mongo_database: '',
-};
--- src/infra/environments/index.ts
+++ src/infra/environments/index.ts
@@ -1,19 +1,19 @@
 import * as dotenv from 'dotenv';
-import { constants } from './constants';
 
 dotenv.config();
-const NODE_ENV: string = process.env.NODE_ENV || constants.node_env;
+const NODE_ENV: string = process.env.NODE_ENV;
 
-const PORT: number = +process.env.PORT || 3000;
+const PORT: number = +process.env.SERVER_PORT || 3000;
 
-const SECRET_JWT = process.env.SECRET_JWT || constants.secret_jwt;
+const SECRET_JWT = process.env.SECRET_JWT;
 
-const MONGO_USER = process.env.MONGO_USER || constants.mongo_user;
-const MONGO_PASS = process.env.MONGO_PASS || constants.mongo_password;
-const MONGO_HOST = process.env.MONGO_HOST || constants.mongo_host;
-const MONGO_DATABASE = process.env.MONGO_DATABASE || constants.mongo_database;
+const MONGO_USER = process.env.DATABASE_USERNAME;
+const MONGO_PASS = process.env.DATABASE_PASSWORD;
+const MONGO_HOST = process.env.DATABASE_HOST;
+const MONGO_DATABASE = process.env.DATABASE_NAME;
+const MONGO_PORT = process.env.DATABASE_PORT;
 const MONGO_URL =
   process.env.MONGO_URL ||
-  `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DATABASE}`;
+  `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`;
 
 export { NODE_ENV, PORT, MONGO_URL, SECRET_JWT };
