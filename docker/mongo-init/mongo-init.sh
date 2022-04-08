0f87e66d986c3fc6c54117618c9e29057ae4be57
Fernando Pucci <fernando.pucci@sensedia.com>
2022-04-08 15:44:02 -0300

    Fix docker structure to automatic init from mongodb

---
+++ docker/mongo-init/mongo-init.sh
@@ -0,0 +1,22 @@
+echo '################ MONGO ENTRYPOINT START ################';
+
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+
+
+print(' Initializing development database... ');
+
+db = db.getSiblingDB('$MONGO_INITDB_DATABASE');
+db.createUser(
+  {
+    user: '$MONGO_INITDB_ROOT_USERNAME',
+    pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+    roles: [{ role: 'readWrite', db: '$MONGO_INITDB_DATABASE' }],
+  },
+);
+db.createCollection('users');
+
+print('Development database created.');
+
+EOF
+
+echo '################ MONGO ENTRYPOINT END ################';
