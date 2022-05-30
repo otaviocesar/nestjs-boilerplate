eada7e73584eb110a535918679bd98ea000e65ca
Otavio Cesar <o.caesar@hotmail.com>
2022-05-30 00:14:57 -0300

    feat: update project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { ValidationPipe, ValidationPipeOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
